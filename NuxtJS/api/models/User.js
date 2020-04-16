33e7ca35f6a5482c285e48d2281ef5488c114561
DirtyyDogg <rbreddels@hotmail.com>
2020-04-16 17:43:43 +0000

    mooi

---
+++ NuxtJS/api/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const User = new Schema ({
+  full_name: { type: String, required: true },
+  email: { 
+      type: String,
+      required: true,
+      lowercase: true,
+      index: { unique: true }
+    },
+  password: { type: String, required: true },
+});
+
+module.exports = mongoose.model('User', User)
