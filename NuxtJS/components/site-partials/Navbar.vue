469b559b1213cb8586f7e5a7ccc45098632fc409
DirtyyDogg <rbreddels@hotmail.com>
2020-04-17 02:25:15 +0000

    ff

--- NuxtJS/components/site-partials/Navbar.vue
+++ NuxtJS/components/site-partials/Navbar.vue
@@ -4,12 +4,12 @@
             <b-navbar-item tag="router-link" :to="{ path: '/' }">
                 <img
                     src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
-                    alt="Lightweight UI components for Vue.js based on Bulma"
+                    alt="Logo Brand"
                 >
             </b-navbar-item>
         </template>
         <template slot="start">
-            <b-navbar-item href="#">
+            <b-navbar-item tag="router-link" :to="{ path: '/' }">
                 Home
             </b-navbar-item>
             <b-navbar-item href="#">
@@ -29,12 +29,20 @@
             v-if="!$auth.loggedIn">
             <b-navbar-item tag="div">
                 <div class="buttons">
-                    <a class="button is-primary">
-                        <strong>Sign up</strong>
-                    </a>
-                    <a class="button is-light">
-                        Log in
-                    </a>
+                    <!--b-navbar-item tag="router-link" :to="{ path: '/' }" class="button is-primary" exact-active-class="active">
+                        <strong>Registreer</strong>                    
+                    </b-navbar-item-->
+                    <nuxt-link class="button is-primary" id="router-link" :to="{ path: '/user/register' }">
+                        <strong>Registreer</strong>                    
+                    </nuxt-link>
+                    
+                    <nuxt-link class="button is-light" to="/user/login" exact-active-class="active">
+                        <strong>Log in</strong>                    
+                    </nuxt-link>
+                    
+                    
+                    
+                    
                 </div>
             </b-navbar-item>
         </template>
